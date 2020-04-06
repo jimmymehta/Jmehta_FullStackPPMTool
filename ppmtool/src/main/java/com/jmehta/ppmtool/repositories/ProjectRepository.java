ac9775a451612278004fc24d3bef3eb01488d5d1
Jimmy Mehta <mehtjimmy@gmail.com>
2020-04-05 18:12:20 -0700

    Project Object fields and repository

---
+++ ppmtool/src/main/java/com/jmehta/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.jmehta.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jmehta.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+	
+
+}
