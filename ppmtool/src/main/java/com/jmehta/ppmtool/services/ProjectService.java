42e910c9834d3d306ba5f735d2c5e1e080ea98d8
Jimmy Mehta <mehtjimmy@gmail.com>
2020-04-06 22:05:03 -0700

    Custom Exception for Project Identifier

--- ppmtool/src/main/java/com/jmehta/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/jmehta/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jmehta.ppmtool.domain.Project;
+import com.jmehta.ppmtool.exceptions.ProjectIdException;
 import com.jmehta.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -13,10 +14,11 @@ public class ProjectService {
 	ProjectRepository projectRepo;
 	
 	public Project saveOrUpdateProject(Project project) {
-		
-		return projectRepo.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepo.save(project);
+		}catch(Exception e) {
+			throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+		}
 	}
-	
-	
-
 }
